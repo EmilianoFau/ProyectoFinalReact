f36c1309da1adf2adc93fa81b9ed3f5e32078209
longuigon <longuigon+git@gmail.com>
2024-10-21 10:33:18 -0300

    register bien hecho

--- ProyectoFinalReact/src/components/Modal/modal.jsx
+++ ProyectoFinalReact/src/components/Modal/modal.jsx
@@ -5,10 +5,13 @@ import Styles from './index.module.css';
 
 const Modal = ({ closeModal }) => {
     const { setUsers } = useUsers();
-
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
+    const [usernameEmpty, setUsernameEmpty] = useState(false);
+    const [passwordEmpty, setPasswordEmpty] = useState(false);
+    const [emailEmpty, setEmailEmpty] = useState(false);
+    const [userExists, setUserExists] = useState(false);
 
     const convertToJson = () => {
         return {
@@ -23,22 +26,31 @@ const Modal = ({ closeModal }) => {
         const newUser = convertToJson();
 
         console.log('New user: ', newUser);
-
-        try {
-            const { response, result } = await postData('http://localhost:3001/api/auth/register', newUser);
-            console.log(response);
-            //Ver que devuelve response y ver logica de agregar usuario
-            /*const addedGame = response[response.length - 1];
-
-            if (addedGame && addedGame.id) {
-                console.log('Juego agregado: ', addedGame);
-                setUsers((prevUsers) => [...prevUsers, addedGame]);
-            }*/
-            console.log(result);
-            closeModal();
-        } catch(error) {
-            console.error("Error: ", error);
+        setUserExists(false);
+        if (username == '' || password == '' || email == '') {
+            username == '' ? setUsernameEmpty(true) : setUsernameEmpty(false);
+            password == '' ? setPasswordEmpty(true) : setPasswordEmpty(false);
+            email == '' ? setEmailEmpty(true) : setEmailEmpty(false);
+        } else {
+            setUsernameEmpty(false);
+            setPasswordEmpty(false);
+            setEmailEmpty(false);
+            setUserExists(false);
+            try {
+                const { response, result } = await postData('http://localhost:3001/api/auth/register', newUser);
+                console.log(response);
+                console.log(result);
+                if (response.status == 400) {
+                    setUserExists(true);
+                } else {
+                    closeModal();
+                }
+            } catch(error) {
+                console.error("Error: ", error);
+            }
         }
+
+        
     }
 
     return (
@@ -49,23 +61,25 @@ const Modal = ({ closeModal }) => {
                     <div>
                         <label>Username</label>
                         <div>
-                            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                            <input className={usernameEmpty ? Styles.emptyField : null} type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
                         </div>
                     </div>
 
                     <div>
                         <label>Password</label>
                         <div>
-                            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <input className={passwordEmpty ? Styles.emptyField : null} type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                         </div>
                     </div>
 
                     <div>
                         <label>Email</label>
                         <div>
-                            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <input className={emailEmpty ? Styles.emptyField : null} type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                         </div>
                     </div>
+
+                    <div className={Styles.alert}>{userExists ? `That email is already registered` : null}</div>
 
                     <div className={Styles.buttons}>
                         <button type="submit" className={Styles.modalButton}>Register</button>
