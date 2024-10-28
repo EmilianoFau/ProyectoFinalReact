854d840125b28ee9f6723a253f54755091a5a5ca
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2024-10-28 08:57:18 -0300

    correcciones

--- ProyectoFinalReact/src/components/Posts/posts.jsx
+++ ProyectoFinalReact/src/components/Posts/posts.jsx
@@ -1,5 +1,12 @@
-export function Posts() {
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUsers } from "../../contexts/users.jsx";
 
+export function Posts() {
+    const [caption, setCaption] = useState('');
+    const [image, setImage] = useState('');
+    
+    
 }
 
 export default Posts;
--- ProyectoFinalReact/src/components/Profile/profile.jsx
+++ ProyectoFinalReact/src/components/Profile/profile.jsx
@@ -1,48 +1,45 @@
-import "./profile.css";
+import Styles from "./index.module.css";
 import { useNavigate } from "react-router-dom";
 
 export function Profile() {
-
     const navigate = useNavigate();
-    return(
+    return (
         <>
-        <div id="headers">
-            <div id="Username">
-                <h2>UnUsername</h2>
+            <div className={Styles.headers}>
+                <div className={Styles.username}>
+                    <h2>UnUsername</h2>
+                </div>
             </div>
-        </div>
-            <div id="container">
-                
-                <div id="profile-container">
-                    <img src="" />
-                    <div id="stats">
+            <div className={Styles.container}>
+                <div className={Styles.profileContainer}>
+                    <img src="" alt="Profile" />
+                    <div className={Styles.stats}>
                         <h5>153</h5>
                         <span>Posts</span>
                         <h5>209</h5>
                         <span>Friends</span>
                     </div>
                 </div>
-                <div id="description">
+                <div className={Styles.description}>
                     <h3>name</h3>
                     <h4>description</h4>
                 </div>
-                <div id="edit">
+                <div className={Styles.edit}>
                     <button>Edit Profile</button>
                 </div>
-                <div id="post-container">
-        
-                </div>
-                <div id="bottom-container">
-                    <button 
+                <div className={Styles.postContainer}></div>
+                <div className={Styles.bottomContainer}>
+                    <button
                         onClick={() => {
                             navigate("/MyFeed");
-                          } } >
-                            Atras
+                        }}
+                    >
+                        Atras
                     </button>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default Profile;
